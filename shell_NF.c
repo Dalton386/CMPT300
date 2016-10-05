cc8bd053104a3f9136eb5f02334604e34a97900c
Dalton <dalton386@outlook.com>
2016-10-05 01:32:15 -0700

    cannot be finished with signals

--- shell_NF.c
+++ shell_NF.c
@@ -7,6 +7,7 @@
 #include <errno.h>
 #include <signal.h>
 #include <stdbool.h>
+#include <termios.h>
 
 #define TOTINS 8
 #define SEPINS 16
@@ -20,15 +21,18 @@ struct WaitingQueue {
 	Ptr2Que next;
 };
 
+struct termios shell_tmodes;
+
 Ptr2Que insertQ(Ptr2Que Wheader, Ptr2Que Wrear, Ptr2Que Wptr);
 Ptr2Que searchQ (Ptr2Que Wheader, int jobid);
+Ptr2Que deleteQ(Ptr2Que Wheader, Ptr2Que Wrear, int jobid);
 int initialQ(Ptr2Que Wheader);
-int deleteQ(Ptr2Que Wheader, int jobid);
 int parse_shell(char (*syscmmd)[SEPINS][ARGLEN]);
-Ptr2Que execute_ntrcmmd(char (*syscmmd)[SEPINS][ARGLEN], Ptr2Que Wheader, Ptr2Que Wrear, Ptr2Que Sheader, Ptr2Que Srear, int *chnptr);
+int execute_ntrcmmd(char (*syscmmd)[SEPINS][ARGLEN], Ptr2Que Wheader, Ptr2Que Wrear, Ptr2Que Sheader, Ptr2Que Srear, Ptr2Que Nrlist[], int *chnptrW, int *chnptrS);
 int xStr2Num(char s[]);
 void initial_syscmmd(char (*syscmmd)[SEPINS][ARGLEN]);
 void execute_process(char (*separate_syscmmd)[ARGLEN]);
+void init_Shell();
 void perror(const char *s);
 // int execute_pipes();
 
@@ -40,10 +44,11 @@ int main (int argc, char const *argv[]) {
 	int ins_cnt, abs_cnt, pipe_cnt;
 	int pfd[2*(TOTINS-1)];
 	int pidlist[TOTINS];
-	int i, j, status, chnptr;
+	int i, j, k;
+	int status, chnptrW, chnptrS;
 	Ptr2Que Wheader, Wrear, Wptr;
 	Ptr2Que Sheader, Srear, Sptr;
-	Ptr2Que Nptr;
+	Ptr2Que Nptr, Nrlist[2];
 	const char *errs; 
 	char readbuffer[1], inputBuffer[1];
 
@@ -53,8 +58,76 @@ int main (int argc, char const *argv[]) {
 	initialQ(Wheader);
 	initialQ(Sheader);
 
+	init_Shell();
 	while (1) {	
-		chnptr = 0;
+		chnptrS = chnptrW = 0;
+
+		Sptr = Sheader;
+		for (i = 0; i < Sptr->pcnt; ++i) {
+			Sptr = Sptr->next;
+			for (j = Sptr->stpid; j < Sptr->pcnt; ++j) {
+				waitpid(Sptr->pidlist[j], &status, WUNTRACED|WCONTINUED|WNOHANG);
+				if(WIFCONTINUED(status) == true) {
+					Nptr = (Ptr2Que)malloc(sizeof(struct WaitingQueue));
+					for (k = 0; k < Sptr->pcnt; ++k) 
+						Nptr->pidlist[k] = Sptr->pidlist[k];
+					Nptr->pcnt = Sptr->pcnt;
+					Nptr->stpid = j;
+					Wrear = insertQ(Wheader,Wrear,Nptr);
+					Srear = deleteQ(Sheader,Srear, i + 1);
+					printf("\nprocess3[%d] has been continued\n", Sptr->pidlist[j]);
+					break;
+				}
+				else if(WIFSTOPPED(status) == true) {
+					printf("\nprocess4[%d] has been stopped\n", Sptr->pidlist[j]);
+					Sptr->stpid = j;
+					break;
+				}
+				else if(WIFEXITED(status) == true) {
+					printf("\nprocess5[%d] has completed\n", Sptr->pidlist[j]);
+				}
+				else if(WIFSIGNALED(status) == true) {
+					printf("job[%d] has been terminated\n", i+1);
+					Srear = deleteQ(Sheader,Srear,i+1);
+					break;
+				}
+			}
+		}
+		Wptr = Wheader;
+		for (i = 0; i < Wptr->pcnt; ++i) {
+			Wptr = Wptr->next;
+			for (j = Wptr->stpid; j < Wptr->pcnt; ++j) {
+				waitpid(Wptr->pidlist[j], &status, WUNTRACED|WNOHANG);
+				if(WIFSTOPPED(status) == true) {
+					Nptr = (Ptr2Que)malloc(sizeof(struct WaitingQueue));
+					for (k = 0; k < Wptr->pcnt; ++k) 
+						Nptr->pidlist[k] = Wptr->pidlist[k];
+					Nptr->pcnt = Wptr->pcnt;
+					Nptr->stpid = j;
+					Srear = insertQ(Sheader,Srear,Nptr);
+					Wrear = deleteQ(Wheader, Wrear, i + 1);
+					printf("\nprocess1[%d] has been stopped\n", Wptr->pidlist[j]);
+					break;
+				}
+				else if(WIFCONTINUED(status) == true) {
+					printf("\nprocess6[%d] has been continued\n", Wptr->pidlist[j]);
+					break;
+				}
+				else if(WIFEXITED(status) == true) {
+					printf("\nprocess5[%d] has completed\n", Wptr->pidlist[j]);
+				}
+				else if(WIFSIGNALED(status) == true) {
+					printf("job[%d] has been terminated\n", i+1);
+					Wrear = deleteQ(Wheader,Wrear,i+1);
+					break;
+				}
+			}
+		}
+
+
+
+
+
 		memset(pidlist,0,sizeof(pidlist));
 		initial_syscmmd(syscmmd);
 		ins_cnt = parse_shell(syscmmd);
@@ -67,47 +140,58 @@ int main (int argc, char const *argv[]) {
 				printf("**%dth is %s\n", i, syscmmd[i][j]);
 		printf("---------------------\n");
 
-		Nptr = execute_ntrcmmd(syscmmd,Wheader,Wrear,Sheader,Srear,&chnptr);
+		Nrlist[0] = NULL;
+		Nrlist[1] = NULL;
+		execute_ntrcmmd(syscmmd,Wheader,Wrear,Sheader,Srear,Nrlist,&chnptrW,&chnptrS);
 																	//cmmd is internal and has been executed successfully
-		if (chnptr == 1) {
-			Wrear = Nptr;
+		if (chnptrW == 1 && chnptrS == 0) {
+			Wrear = Nrlist[0];
 			continue;
 		}									
-		else if (chnptr == 2) {
-			Srear = Nptr;
+		else if (chnptrW == 0 && chnptrS == 1) {
+			Srear = Nrlist[1];
 			continue;
 		}
-		else if (chnptr == -1) {
+		else if (chnptrW == 1 && chnptrS == 1) {
+			Wrear = Nrlist[0];
+			Srear = Nrlist[1];
 			continue;
-		}									
+		}
+		else if (chnptrW == 0 && chnptrS == 0) {
+			continue;
+		}			
+		else if (chnptrW == -1 && chnptrS == -1) {
+			printf("not an internal cmmd\n");
+		}				
 
 		for (i = 0; i < pipe_cnt; ++i)
 			pipe(pfd+2*i);	
 
 		for (i = 0; i < abs_cnt; ++i)
 		{
-			printf("0\n");
 			pid = fork();
-			printf("1\n");
 			pidlist[i] = pid;
-			printf("i is %d, pid is %d\n", i, pid);
 			if (pid < 0) {
 				fprintf(stderr, "Fork Failed");
 				return 1;
 			}
 			else if (pid == 0) {
-				kill(getpid(), SIGSTOP);
-				write(pfd[1], inputBuffer, 0);
+				signal (SIGINT, SIG_DFL);
+			    signal (SIGQUIT, SIG_DFL);
+			    signal (SIGTSTP, SIG_DFL);
+			    signal (SIGTTIN, SIG_DFL);
+			    signal (SIGTTOU, SIG_DFL);
+			    signal (SIGCHLD, SIG_DFL);
 				if (i == 0 && i != abs_cnt-1) {
-					printf("2\n");
-					dup2(pfd[1], 1);
+					close(STDOUT_FILENO);
+					dup2(pfd[1], STDOUT_FILENO);
 					for (j = 0; j < pipe_cnt; ++j) {				//prevent the leak of open file descripters
 						close(pfd[2*j]);
 						close(pfd[2*j + 1]);
 					}
 				}
 				else if (i != 0 && i == abs_cnt-1) {
-					printf("3\n");		
+					close(STDIN_FILENO);
 					dup2(pfd[2*(i-1)], STDIN_FILENO);
 					for (j = 0; j < pipe_cnt; ++j) {
 						close(pfd[2*j]);
@@ -115,7 +199,8 @@ int main (int argc, char const *argv[]) {
 					}		
 				}
 				else if (i != 0 && i != abs_cnt-1) {
-					printf("4\n");
+					close(STDIN_FILENO);
+					close(STDOUT_FILENO);
 					dup2(pfd[2*(i-1)], STDIN_FILENO);
 					dup2(pfd[2*i + 1], STDOUT_FILENO);
 					for (j = 0; j < pipe_cnt; ++j) {
@@ -123,14 +208,9 @@ int main (int argc, char const *argv[]) {
 						close(pfd[2*j + 1]);
 					}
 				}
-
 				execute_process(syscmmd[i]);
 			}
-			printf("xxxxxxxxxxxxxxxxxxxx\n");
-		}
-		for (i = 0; i < abs_cnt; ++i) {
-			kill(pidlist[i], SIGCONT);
-			read(pfd[0], readbuffer, 0);
+
 		}
 		for (i = 0; i < pipe_cnt; ++i) {
 			close(pfd[2*i]);
@@ -138,26 +218,22 @@ int main (int argc, char const *argv[]) {
 		}
 		if (ins_cnt > 0){
 			for (i = 0; i < abs_cnt; ++i) {
-				printf("5\n");
 				waitpid(pidlist[i], &status, WUNTRACED);
-				printf("6\n");
 				if(WIFSTOPPED(status) == true) {
-					printf("7\n");
 					Sptr = (Ptr2Que)malloc(sizeof(struct WaitingQueue));
 					for (j = 0; j < abs_cnt; ++j) 
 						Sptr->pidlist[j] = pidlist[j];
 					Sptr->pcnt = abs_cnt;
 					Sptr->stpid = i;
 					Srear = insertQ(Sheader,Srear,Sptr);
-					printf("process[%d] has been stopped\n", pidlist[i] );
+					printf("\nprocess[%d] has been stopped\n", pidlist[i] );
 					break;
 				}
 				else
-					printf("process[%d] has completed\n", pidlist[i]);
-				printf("8\n");
+					printf("\nprocess[%d] has completed\n", pidlist[i]);
 			}
 		}
-		else {
+		else if(ins_cnt < 0) {
 			Wptr = (Ptr2Que)malloc(sizeof(struct WaitingQueue));
 			for (i = 0; i < abs_cnt; ++i) 
 				Wptr->pidlist[i] = pidlist[i];
@@ -168,7 +244,46 @@ int main (int argc, char const *argv[]) {
 			printf("Let's continue!\n");
 		}
 	}
+
 	return 0;
+}
+
+void init_Shell() {
+	int shell_terminal;
+	int shell_is_interactive;
+	pid_t shell_pgid;
+
+	shell_terminal = STDIN_FILENO;
+  	shell_is_interactive = isatty (shell_terminal);
+
+  	if (shell_is_interactive)
+    {
+      /* Loop until we are in the foreground.  */
+      while (tcgetpgrp (shell_terminal) != (shell_pgid = getpgrp ()))
+        kill (- shell_pgid, SIGTTIN);
+
+      /* Ignore interactive and job-control signals.  */
+      // signal (SIGINT, SIG_IGN);
+      signal (SIGQUIT, SIG_IGN);
+      signal (SIGTSTP, SIG_IGN);
+      signal (SIGTTIN, SIG_IGN);
+      signal (SIGTTOU, SIG_IGN);
+      signal (SIGCHLD, SIG_IGN);
+
+      /* Put ourselves in our own process group.  */
+      shell_pgid = getpid ();
+      if (setpgid (shell_pgid, shell_pgid) < 0)
+        {
+          perror ("Couldn't put the shell in its own process group");
+          exit (1);
+        }
+
+      /* Grab control of the terminal.  */
+      tcsetpgrp (shell_terminal, shell_pgid);
+
+      /* Save default terminal attributes for shell.  */
+      tcgetattr (shell_terminal, &shell_tmodes);
+    }
 }
 
 int parse_shell(char (*syscmmd)[SEPINS][ARGLEN]) {
@@ -177,10 +292,13 @@ int parse_shell(char (*syscmmd)[SEPINS][ARGLEN]) {
 	char *saveptr1, *saveptr2;
 	char *delim1, *delim2;
 	int i,j;
+	char hostname[80];
 
 	delim1 = "|\n\r\f\v";
 	delim2 = "\t ";								//maybe not right.
 
+	gethostname(hostname,sizeof(hostname));
+	printf("\n[%s]$ ", hostname);
 	fgets(cmmd_input, sizeof(cmmd_input), stdin);
 	str1 = cmmd_input;
 	i = 0;
@@ -190,14 +308,10 @@ int parse_shell(char (*syscmmd)[SEPINS][ARGLEN]) {
 		token = strtok_r(str1,delim1,&saveptr1);
 		if (!token) 
 			break;
-		if (i != 0) 
-			strcpy (syscmmd[i][2],"STDIN");
 		
 		for (str2 = token ;; str2 = NULL)
 		{
 			j++;
-			if (j == 2 && i != 0) 
-				continue;
 			subtoken = strtok_r(str2,delim2,&saveptr2);
 			if (!subtoken)
 				break;
@@ -229,16 +343,12 @@ void execute_process(char (*separate_syscmmd)[ARGLEN]) {
 	char *arg[SEPINS];
 	int j;
 
-	for (j = 1; j <= separate_syscmmd[0][0]; ++j) {
-		if (strcmp(separate_syscmmd[j], "STDIN") == 0)
-			arg[j-1] = STDIN_FILENO;		
-		else
-			arg[j-1] = separate_syscmmd[j];		
-	}
+	for (j = 1; j <= separate_syscmmd[0][0]; ++j) 
+		arg[j-1] = separate_syscmmd[j];		
 
 	arg[j-1] = NULL;
-	printf("execute, process is %s\n", separate_syscmmd[1]);
 	execvp(separate_syscmmd[1],arg);
+	exit(1);
 }
 
 int initialQ(Ptr2Que Wheader) {
@@ -250,32 +360,37 @@ int initialQ(Ptr2Que Wheader) {
 	return 0;
 }
 
-int deleteQ (Ptr2Que Wheader, int jobid) {
+Ptr2Que deleteQ (Ptr2Que Wheader, Ptr2Que Wrear, int jobid) {
 	Ptr2Que Wptr_f, Wptr_l, Wptr_n;
 	int i;
 
-	if (Wheader->next == NULL)
-		return 0;
+	if (Wheader->next == NULL) {
+		printf("the target job doesn't exist\n");
+		return Wheader;
+	}
 
 	Wptr_f = Wheader;										//the first job 
 	Wptr_l = Wptr_f->next;
-	if (Wptr_l == NULL)
-		return 1;
 
 	for (i = 1; i < jobid; ++i) {
 		Wptr_f = Wptr_f->next;
 		Wptr_l = Wptr_f->next;
-		if (Wptr_l == NULL)
-			return 1;										//the target job doesn't exist
+		if (Wptr_l == NULL) {
+			printf("\nthe target job doesn't exist\n");
+			return Wrear;										//the target job doesn't exist
+		}
 	}
 
 	Wptr_n = Wptr_l->next;
 	Wptr_f->next = Wptr_n;
 	Wheader->pcnt--;
 
+	if(Wptr_f->next == NULL)
+		return Wptr_f;
+
 	free(Wptr_l);
 
-	return 0;
+	return Wrear;
 }
 
 Ptr2Que insertQ(Ptr2Que Wheader, Ptr2Que Wrear, Ptr2Que Wptr) {
@@ -317,16 +432,15 @@ int xStr2Num(char s[]) {
 	return number;
 }
 
-Ptr2Que execute_ntrcmmd(char (*syscmmd)[SEPINS][ARGLEN], Ptr2Que Wheader, Ptr2Que Wrear, Ptr2Que Sheader, Ptr2Que Srear, int *chnptr) {
+int execute_ntrcmmd(char (*syscmmd)[SEPINS][ARGLEN], Ptr2Que Wheader, Ptr2Que Wrear, Ptr2Que Sheader, Ptr2Que Srear, Ptr2Que Nrlist[], int *chnptrW, int *chnptrS) {
 	int i,j;
-	int jobid, status, is_stopped;
-	Ptr2Que Sptr, Wptr, Nptr, Nrear;
+	int jobid, status;
+	Ptr2Que Sptr, Wptr, Nptr;
 
 	Sptr = Sheader;
 	Wptr = Wheader;
-	Nptr = Nrear = NULL;
-	is_stopped = 1;
-	*chnptr = -1;
+	Nptr = NULL;
+	*chnptrW = *chnptrS = 0;
 	if(strcmp(syscmmd[0][1], "exit") == 0) {					// exit
 		exit(0);
 	}
@@ -341,9 +455,20 @@ Ptr2Que execute_ntrcmmd(char (*syscmmd)[SEPINS][ARGLEN], Ptr2Que Wheader, Ptr2Qu
 			if (Wptr->next == NULL)
 				break;
 			Wptr = Wptr->next;
-			printf("[%d]             1st process is %d\n", i+Sheader->pcnt, Wptr->pidlist[0]);
+			for (j = Wptr->stpid; j < Wptr->pcnt; ++j) {
+				if (waitpid(Wptr->pidlist[j], &status, WNOHANG) == 0) {
+					printf("[%d] Running     1st process is %d\n", i+Sheader->pcnt, Wptr->pidlist[0]);
+					Wptr->stpid = j;
+					break;
+				}
+				if (j == Wptr->pcnt - 1) {
+					printf("[%d] Done        1st process is %d\n", i+Sheader->pcnt, Wptr->pidlist[0]);
+					Wrear = deleteQ(Wheader,Wrear,i);
+					Nrlist[0] = Wrear;
+					*chnptrW = 1;
+				}
+			}
 		}
-
 	}
 	else if(strcmp(syscmmd[0][1], "cd") == 0) {					//cd
 		chdir(syscmmd[0][2]);
@@ -359,49 +484,63 @@ Ptr2Que execute_ntrcmmd(char (*syscmmd)[SEPINS][ARGLEN], Ptr2Que Wheader, Ptr2Qu
 			Sptr = searchQ(Sheader,jobid);
 			if (Sptr == NULL) {
 				printf("There is no such job\n");
-				return NULL;
+				return 0;
 			}
 
 			for (i = Sptr->stpid; i < Sptr->pcnt; ++i) {
-				waitpid(Sptr->pidlist[0], &status, WUNTRACED);
+				while (i == Sptr->stpid) {
+					kill(Sptr->pidlist[Sptr->stpid], SIGCONT);
+					waitpid(Sptr->pidlist[Sptr->stpid], &status, WCONTINUED);
+					if(WIFCONTINUED(status))
+						break;
+				}				
+				waitpid(Sptr->pidlist[i], &status, WUNTRACED);
 				if(WIFSTOPPED(status) == true) {
-					is_stopped = 0;
 					Sptr->stpid = i;
-					printf("process[%d] has been stopped\n", Sptr->pidlist[i]);
+					printf("\nprocess[%d] has been stopped\n", Sptr->pidlist[i]);
 					break;
 				}
 				else
-					printf("process[%d] has completed\n", Sptr->pidlist[i]);
+					printf("\nprocess[%d] has completed\n", Sptr->pidlist[i]);
 			}
 
-			if (is_stopped != 0)
-				deleteQ(Sheader,jobid);
+			if (i == Sptr->pcnt) {
+				Srear = deleteQ(Sheader,Srear,jobid);
+				Nrlist[1] = Srear;
+				*chnptrS = 1;
+				printf("\njob[%d] has completed\n", jobid);
+			}
 		}
 		else {
 			Wptr = searchQ(Wheader,jobid - Sheader->pcnt);
 			if (Wptr == NULL) {
 				printf("There is no such job\n");
-				return NULL;
+				return 0;
 			}
 
 			for (i = Wptr->stpid; i < Wptr->pcnt; ++i) {
-				waitpid(Wptr->pidlist[0], &status, WUNTRACED);
+				waitpid(Wptr->pidlist[i], &status, WUNTRACED);
 				if(WIFSTOPPED(status) == true) {
 					Nptr = (Ptr2Que)malloc(sizeof(struct WaitingQueue));
 					for (j = 0; j < Wptr->pcnt; ++j) 
 						Nptr->pidlist[j] = Wptr->pidlist[j];
 					Nptr->pcnt = Wptr->pcnt;
 					Nptr->stpid = i;
-					Nrear = insertQ(Sheader,Srear,Nptr);
-					*chnptr = 2;
-					printf("process[%d] has been stopped\n", Wptr->pidlist[i]);
+					Srear = insertQ(Sheader,Srear,Nptr);
+					Nrlist[1] = Srear;
+					*chnptrS = 1;
+					printf("\nprocess[%d] has been stopped\n", Wptr->pidlist[i]);
 					break;
 				}
 				else
-					printf("process[%d] has completed\n", Wptr->pidlist[i]);
+					printf("\nprocess[%d] has completed\n", Wptr->pidlist[i]);
 			}
-
-			deleteQ(Wheader,jobid - Sheader->pcnt);
+			if (i == Wptr->pcnt) {
+				Wrear = deleteQ(Wheader,Wrear,jobid - Sheader->pcnt);
+				Nrlist[0] = Wrear;
+				*chnptrW = 1;
+				printf("\njob[%d] has completed\n", jobid);
+			}
 		}
 
 	}
@@ -413,7 +552,7 @@ Ptr2Que execute_ntrcmmd(char (*syscmmd)[SEPINS][ARGLEN], Ptr2Que Wheader, Ptr2Qu
 
 		if (Sheader->pcnt < jobid) {
 			printf("There is no such job\n");
-			return NULL;
+			return 0;
 		}
 
 		Sptr = searchQ(Sheader,jobid);
@@ -422,16 +561,23 @@ Ptr2Que execute_ntrcmmd(char (*syscmmd)[SEPINS][ARGLEN], Ptr2Que Wheader, Ptr2Qu
 			Nptr->pidlist[j] = Sptr->pidlist[j];
 		Nptr->pcnt = Sptr->pcnt;
 		Nptr->stpid = Sptr->stpid;
-		Nrear = insertQ(Wheader,Wrear,Nptr);
-		*chnptr = 1;
-		deleteQ(Sheader,jobid);
-
-		kill(Nptr->pidlist[Nptr->stpid], SIGCONT);
+		Wrear = insertQ(Wheader,Wrear,Nptr);
+		Nrlist[0] = Wrear;
+		*chnptrW = 1;
+		Srear = deleteQ(Sheader,Srear,jobid);
+		Nrlist[1] = Srear;
+		*chnptrS = 1;
+		while (1) {
+			kill(Nptr->pidlist[Nptr->stpid], SIGCONT);
+			waitpid(Nptr->pidlist[Nptr->stpid], &status, WCONTINUED|WUNTRACED|WNOHANG);
+			if(WIFCONTINUED(status)) {
+				printf("process[%d] has been continued\n", Nptr->pidlist[Nptr->stpid]);
+				break;
+			}
+		}
 	}
 	else {
-		*chnptr = 0;
-		return NULL;												//cmmd is not internal 
+		*chnptrW = *chnptrS = -1;												//cmmd is not internal 
 	}
 
-	return Nrear;
 }
